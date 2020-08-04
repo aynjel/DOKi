870fe26914dc2edce5a6bb7f8f20828054fea37f
Kristoffer Dominic Amora <kristoffer_dominic_amora@chonghua.com.ph>
2020-08-04 08:35:59 +0800

    Revert http API calls to https in prod environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://10.128.18.75:8082/api/',
-  apiUrlDoctorsPortal: 'http://10.128.18.75:8088/api/'
+  apiUrl: 'https://10.128.18.75:8082/api/',
+  apiUrlDoctorsPortal: 'https://10.128.18.75:8088/api/'
 };
