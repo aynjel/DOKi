0163232b3f9bae43801f1fb47c99899c3a3ed9a1
Kristoffer Dominic Amora <kristoffer_dominic_amora@chonghua.com.ph>
2020-08-06 12:06:02 +0800

    Quick and dirty fix for node server app calling web api proof of concept.

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,8 @@ var logger = require("morgan");
 var methodOverride = require("method-override");
 var cors = require("cors");
 
-var https = require("https");
+//var https = require("https");
+//const request = require('request');
 
 var app = express();
 app.use(logger("dev"));
@@ -14,37 +15,57 @@ app.use(cors());
 
 app.get("/posts", function (req, res) {
   console.log("posts!");
+  //let r;
 
   // call api
   //MD000175
   //02/08/1954
 
  //req.send "http://10.128.18.75:8088/api/Login/Get?drcode=MD000175&birthday=02%2F08%2F1954"
+ // https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
 
 
 //==================================
-//const https = require('http');
+  const http = require('http');
 
-https.get('https://swapi.dev/api/people/1', (res) => {
-  let data = '';
+  http.get('http://10.128.18.75:8088/api/Login/Get?drcode=MD000175&birthday=02%2F08%2F1954', (response) => {
+    let data = '';
 
-  // A chunk of data has been recieved.
-  res.on('data', (chunk) => {
-    data += chunk;
+      // A chunk of data has been recieved.
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      response.on('end', () => {
+        console.log(data);
+        //console.log(JSON.parse(data).explanation);
+
+        //res.send(response);
+        //res.json(response).send();
+        res.send(data);
+        
+      });
+
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
   });
-
-  // The whole response has been received. Print out the result.
-  res.on('end', () => {
-    //console.log(JSON.parse(data).explanation);
-    console.log(data);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
 //==================================
 
-  return res;
 });
 
 app.listen(process.env.PORT || 8085);
+
+// // using Request
+// const request = require('request');
+
+// //request({url: 'http://example.com', proxy: 'https://host:port'}, (error, response, body) => {
+
+// request({url: 'http://10.128.18.75:8088/api/Login/Get?drcode=MD000175&birthday=02%2F08%2F1954', proxy: 'http://localhost:8085'}, { json: true }, (err, response, body) => {
+//   if (err) { return console.log(err); }
+//   console.log(body.url);
+//   console.log(body.explanation);
+// });
+// console.log(response);
+  
+//   res.send(response);
