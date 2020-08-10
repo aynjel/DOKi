0cc5ae78df799b445e8b9f3729b0086da3302d7c
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2020-08-10 13:49:16 +0800

    fix error message

--- src/app/page/login/login.page.ts
+++ src/app/page/login/login.page.ts
@@ -60,12 +60,19 @@ export class LoginPage implements OnInit {
         this.btnDisable = true;
         this.authService.doctorsPortalLogin(this.postData.username, this.postData.password).subscribe(
           (res: any) => {
+            console.log('res :');
             console.log(res);
-            if(res != ""){
+            if(res.length != "0"){
+
+              if(res.Message){
+                this.Alert(res.Message,'Okay');
+              }else{
+
+         
               this.logindata = <LoginData>res;
               this.storageService.store(AuthConstants.AUTH, this.logindata);
-
               this.router.navigate(['/menu/dashboard']);
+            }
             }else{
               this.Alert('Incorrect Authentication Details.','Okay');
               //this.toast.presentToast('Incorrect Authentication Details.');
