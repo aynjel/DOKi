29869cab058c1d488ddee7a5d24a3fac0382fb2a
Roberto Pedroza Jr <roberto_pedroza@chonghua.com.ph>
2020-09-30 15:59:18 +0800

    Test Landing Page and Slider

--- cypress/integration/e2e/landingPage.spec.js
+++ cypress/integration/e2e/landingPage.spec.js
@@ -1,39 +1,40 @@
 context('Landing', () => {
+
         beforeEach(() => {
             cy.visit("/");
         });
 
-        
-      it("Continue Swipe", () => {
+      it("Navigate", () => {
 
-        cy.get('ion-slides')
-            .trigger('pointerdown', { which: 1 })
-            .trigger('pointermove',{clientX: 100, clientY: 275})
-            .trigger('pointerup', { force: true });
+        cy.get("ion-grid");
         
-            cy.get('ion-slides')
-            .trigger('mousedown', {which: 2})
-            .trigger('mousemove', {clientX: 100, clientY: 275})
-            .trigger('mouseup', {force: true});
- 
-            //cy.swipeLeft();
-            //cy.swipeLeft();
-            //cy.swipeLeft();
+         // Left To Right
+        cy.firstSlideGoRight();
+        cy.wait(1000);
+        cy.secondSlideGoRight();
+        cy.wait(1000);
+        cy.thirdSlideGoRight();
+        cy.wait(1000);
+
+         // Right To Left
+        cy.fourthSlideGoLeft();
+        cy.wait(1000);
+        cy.thirdSlideGoLeft();
+        cy.wait(1000);
+        cy.secondSlideGoLeft();
+        cy.wait(1000);
+
+         // Left To Right
+         cy.firstSlideGoRight();
+         cy.wait(1000);
+         cy.secondSlideGoRight();
+         cy.wait(1000);
+         cy.thirdSlideGoRight();
+         cy.wait(3000);
+
+         // Click Let's Go Button
+         //cy.get('ion-button[id="button-lets-go"]').click();
 
         });
-
+    
  });
-
-/*  describe("Form Test", () => {
-   
-    it("Can fill the form", () => {
-        cy.visit("/");
-      });
- }); */
-
-/*  Cypress.Commands.add('swipeLeft', () => {
-    cy.get('.swiper-slide-active')
-      .trigger('mousedown', {position: "right"})
-      .trigger('mousemove', {clientX: 100, clientY: 275})
-      .trigger('mouseup', {force: true})
-  }); */
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,3 +30,56 @@ Cypress.Commands.add('swipeLeft', () => {
       .trigger('mousemove', {clientX: 100, clientY: 275})
       .trigger('mouseup', {force: true})
   });
+
+/**
+ * Slider Commands
+ */
+// First Slide
+Cypress.Commands.add('firstSlideGoRight', () => {
+    cy.get('ion-button[id="first-slide-button-right"]').click();
+  });
+
+// Second Slide
+Cypress.Commands.add('secondSlideGoRight', () => {
+    cy.get('ion-button[id="second-slide-button-right"]').click();
+  });
+  Cypress.Commands.add('secondSlideGoLeft', () => {
+    cy.get('ion-button[id="second-slide-button-left"]').click();
+  });
+
+// Third Slide
+  Cypress.Commands.add('thirdSlideGoRight', () => {
+    cy.get('ion-button[id="third-slide-button-right"]').click();
+  });
+  Cypress.Commands.add('thirdSlideGoLeft', () => {
+    cy.get('ion-button[id="third-slide-button-left"]').click();
+  });
+
+// Fourth Slide
+Cypress.Commands.add('fourthSlideGoLeft', () => {
+    cy.get('ion-button[id="fourth-slide-button-left"]').click();
+  });
+
+// Left To Right
+Cypress.Commands.add('leftToRight', () => {
+    // Left To Right
+    cy.firstSlideGoRight();
+    cy.wait(1000);
+    cy.secondSlideGoRight();
+    cy.wait(1000);
+    cy.thirdSlideGoRight();
+    cy.wait(1000);
+  });
+
+  Cypress.Commands.add('rightToLeft', () => {
+     // Right To Left
+     cy.fourthSlideGoLeft();
+     cy.wait(1000);
+     cy.thirdSlideGoLeft();
+     cy.wait(1000);
+     cy.secondSlideGoLeft();
+     cy.wait(1000);
+  });
+
+  
+
