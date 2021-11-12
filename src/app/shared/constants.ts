7959be0061e7dd06f7c8057d093e8d4c24e4801f
Kristoffer Dominic Amora <kristoffer_dominic_amora@chonghua.com.ph>
2021-11-12 14:30:39 +0800

    Bump up version number.

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = '2.0.12';
+  public DOCTOR_PORTAL__VALUE__VERSION: string = '2.1.0';
 
   // (W)
   /**
