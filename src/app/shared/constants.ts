8865821151e6fd31cb4bbf9bf517c21e19c94ae0
Kristoffer Dominic Amora <kristoffer_dominic_amora@chonghua.com.ph>
2021-11-12 14:10:36 +0800

    Update version number.

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = '2.0.10';
+  public DOCTOR_PORTAL__VALUE__VERSION: string = '2.0.12';
 
   // (W)
   /**
