1e6326acd25a46422eb343e136b1db299b17922c
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2021-12-06 16:17:46 +0800

    updated login page info

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -126,7 +126,7 @@ export class Constants {
   public UI_COMPONENT_TEXT__VALUE__SETTINGS_APPEARANCE_DARK_MODE: string =
     'Switch to Dark Mode';
   public UI_COMPONENT_TEXT__VALUE__SETTINGS_INFO: string =
-    'For inquiries, please contact Systems Solution & Business Intelligence Team of Chong Hua Hospital IT Division';
+    'For inquiries, please contact DOKi App Project Team';
   public UI_COMPONENT_TEXT__VALUE__SETTINGS_INFO_TELEPHONE: string =
     '+63 32 233 8000 local 8888';
   public UI_COMPONENT_TEXT__VALUE__SETTINGS_INFO_TELEPHONE_HREF: string =
