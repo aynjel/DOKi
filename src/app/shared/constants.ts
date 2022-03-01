aef73bb74b8f8e9e068940e3f018c2876dc0edcb
Kristoffer Dominic Amora <kristoffer_dominic_amora@chonghua.com.ph>
2022-03-01 18:13:50 +0800

    Version 2.4.9.

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = '2.4.4';
+  public DOCTOR_PORTAL__VALUE__VERSION: string = '2.4.9';
 
   // (W)
   /**
