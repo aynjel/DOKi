5c8bc130e2540218cf1209561f579b7821533637
Mark D. Sibi <mark_sibi@chonghua.com.ph>
2022-04-13 14:21:38 +0800

    Updated testUseraccounts list

--- cypress/integration/e2e/1-cym-login-page.spec.js
+++ cypress/integration/e2e/1-cym-login-page.spec.js
@@ -25,13 +25,13 @@ describe("Actions 1 - Mobile Login Page", () => {
     });
 
     it("3. Test Scenario 3 - Invalid Username", () => {
-        cy.errLogin(userAccount[0].userName, userAccount[0].password, "invalid-user")
+        cy.errLogin(userAccount[3].userName, userAccount[3].password, "invalid-user")
 
         cy.end()
     }); 
 
     it("4. Test Scenario 4 - Invalid Password", () => {
-        cy.errLogin(userAccount[0].userName, userAccount[0].password, "invalid-pass")
+        cy.errLogin(userAccount[2].userName, userAccount[2].password, "invalid-pass")
 
         cy.end()
     });
