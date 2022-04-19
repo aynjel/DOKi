8fb6f9cc301eb8a2646e0d7e0cf2f4b8f48f6250
Mark D. Sibi <mark_sibi@chonghua.com.ph>
2022-04-19 15:53:15 +0800

    Updated offline cypress test script

--- cypress/integration/e2e/11-cym-offline-test.spec.js
+++ cypress/integration/e2e/11-cym-offline-test.spec.js
@@ -1,17 +1,19 @@
 /// <reference types='cypress'/>
 
+// https://chromedevtools.github.io/devtools-protocol/1-3/Network/#method-emulateNetworkConditions
+// https://github.com/cypress-io/cypress/issues/17723
+
 const goOffline = () => {
     cy.log('**go offline**')
-        // .then(() => {
-        //     return Cypress.automation('remote:debugger:protocol',
-        //         {
-        //             command: 'Network.enable',
-        //         })
-        // })
         .then(() => {
-            return Cypress.automation('remote:debugger:protocol',
+            Cypress.automation('remote:debugger:protocol',
                 {
                     command: 'Network.enable',
+                })
+        })
+        .then(() => {
+            Cypress.automation('remote:debugger:protocol',
+                {
                     command: 'Network.emulateNetworkConditions',
                     params: {
                         offline: true,
@@ -22,30 +24,29 @@ const goOffline = () => {
                 })
         })
 }
-// https://github.com/cypress-io/cypress/issues/17723
+
 const goOnline = () => {
     // disable offline mode, otherwise we will break our tests :)
     cy.log('**go online**')
+        .then(() => {
+            Cypress.automation('remote:debugger:protocol',
+                {
+                    command: 'Network.emulateNetworkConditions',
+                    params: {
+                        offline: false,
+                        latency: -1,
+                        downloadThroughput: -1,
+                        uploadThroughput: -1,
+                    },
+                })
+        })
     .then(() => {
-      // https://chromedevtools.github.io/devtools-protocol/1-3/Network/#method-emulateNetworkConditions
-      return Cypress.automation('remote:debugger:protocol',
+      Cypress.automation('remote:debugger:protocol',
         {
-          command: 'Network.emulateNetworkConditions',
-          params: {
-            offline: false,
-            latency: -1,
-            downloadThroughput: -1,
-            uploadThroughput: -1,
-          },
+          command: 'Network.disable',
         })
     })
-    // .then(() => {
-    //   return Cypress.automation('remote:debugger:protocol',
-    //     {
-    //       command: 'Network.disable',
-    //     })
-    // })
-  }
+}
 
 var userAccount;
 
@@ -58,21 +59,24 @@ describe("Actions", () => {
             cy.fixture('testUserAccount').then(function (data) {
                 userAccount = data;
             })
-            goOnline()
+            // goOffline()
+            // goOnline()
         });
 
         afterEach(() => {
-            goOnline()
-            cy.reload()
-            cy.url({ timeout: 10000 }).should("include", Cypress.env("loginUrl"))
+            // goOnline()
+            // cy.reload()
+            // cy.url({ timeout: 10000 }).should("include", Cypress.env("loginUrl"))
         })
 
         it("Test Scenario 1 - Login then Set offline", () => {
             cy.login(userAccount[0].userName, userAccount[0].password, true)
 
-            goOffline()
+            // goOffline()
 
             cy.contains("Dashboard")
+
+            // goOnline()
 
             cy.end()
         });
