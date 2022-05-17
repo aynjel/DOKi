c3f7d94d8c9ad9052ee44132896450549c190a15
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2022-05-17 13:49:52 +0800

    unified back button on index, updated version to 3.0.12

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.0.11';
+  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.0.12';
 
   // (W)
   /**
