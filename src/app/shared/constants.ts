938563eed13c580ed8d7a47696539c839cbbf547
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2022-05-17 14:05:21 +0800

    updated version to 3.0.13 from 12

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.0.12';
+  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.0.13';
 
   // (W)
   /**
