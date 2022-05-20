25e442e05b1f864080939f7da3c3765849090f9d
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2022-05-20 14:03:39 +0800

    updated version to 3.2.5

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.2.4';
+  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.2.5';
 
   // (W)
   /**
