d0629cd88adc0910b81d742b8f9e443b09014694
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2022-07-04 09:56:58 +0800

    uped version 3.2.6 to 3.2.7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,8 @@ export const environment = {
   // apiRouterUrl: 'https://doctorsportal.chonghua.com.ph/api/',
   apiRouterUrl: '/api/', // For IIS Deployment where Frontend and Backend are deployed in one server.
   linkRouterUrl: 'https://doki.chonghua.com.ph/',
+  //apiRouterUrlTest: 'http://localhost:5000',
+  apiRouterUrlTest: 'https://api01.chonghua.com.ph',
   // apiRouterUrl: 'http://10.128.18.112:9091/api/',
-  consoleLog: false
+  consoleLog: false,
 };
