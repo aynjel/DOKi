8866263c745b683e00b02f74160f9229eae00062
Jessie Rubi <jessie_jay_rubi@chonghua.com.ph>
2022-09-06 15:34:13 +0800

    bumped version

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.2.8';
+  public DOCTOR_PORTAL__VALUE__VERSION: string = '3.2.9';
 
   // (W)
   /**
