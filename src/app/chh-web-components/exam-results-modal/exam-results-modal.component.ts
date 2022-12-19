25acf6d92512bed3a6bd6e979a3e135b873c795c
Jessie Jay Rubi <jessie_jay_rubi@chonghua.com.ph>
2022-12-19 15:00:46 +0800

    test

--- src/app/chh-web-components/exam-results-modal/exam-results-modal.component.ts
+++ src/app/chh-web-components/exam-results-modal/exam-results-modal.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, Input, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
 
-import { takeUntil } from 'rxjs/operators';
-import { BehaviorSubject, Subject } from 'rxjs';
-import { OnlineResultService } from 'src/app/services/onlie-result/online-result.service';
+import { takeUntil } from "rxjs/operators";
+import { BehaviorSubject, Subject } from "rxjs";
+import { OnlineResultService } from "src/app/services/onlie-result/online-result.service";
 @Component({
-  selector: 'app-exam-results-modal',
-  templateUrl: './exam-results-modal.component.html',
-  styleUrls: ['./exam-results-modal.component.scss'],
+  selector: "app-exam-results-modal",
+  templateUrl: "./exam-results-modal.component.html",
+  styleUrls: ["./exam-results-modal.component.scss"],
 })
 export class ExamResultsModalComponent implements OnInit {
   private ngUnsubscribe = new Subject();
@@ -18,14 +18,14 @@ export class ExamResultsModalComponent implements OnInit {
   @Input() ExamDetails;
   @Input() requestType;
   pdf = {
-    requestNo: 'string',
-    patientNo: 'string',
-    section: 'string',
-    examType: 'string',
-    examCode: 'string',
-    examNo: 'string',
-    testId: 'string',
-    site: 'string',
+    requestNo: "string",
+    patientNo: "string",
+    section: "string",
+    examType: "string",
+    examCode: "string",
+    examNo: "string",
+    testId: "string",
+    site: "string",
   };
   ngOnInit() {
     console.log(this.requestType);
@@ -43,7 +43,7 @@ export class ExamResultsModalComponent implements OnInit {
   link;
   isPDFLoading: boolean = false;
   getPDF(data) {
-    console.log('getPDF');
+    console.log("getPDF");
     this.isPDFLoading = true;
     this.orService
       .getPDF(this.pdf, this.requestType)
@@ -53,13 +53,17 @@ export class ExamResultsModalComponent implements OnInit {
           console.log(data);
 
           this.isPDFLoading = false;
-          let blob = new Blob([data], { type: 'application/pdf' });
+          let blob = new Blob([data], { type: "application/pdf" });
           let downloadURL = window.URL.createObjectURL(data);
           this.pdfSrc = downloadURL;
-          this.link = '';
-          this.link = document.createElement('a');
+          this.link = "";
+          this.link = document.createElement("a");
           this.link.href = downloadURL;
-          this.link.download = 'PFRegular.pdf';
+          this.link.download =
+            this.ExamDetails.exam_Description +
+            "-" +
+            this.ExamDetails.date_Exam +
+            ".pdf";
         },
         (error) => {
           this.isPDFLoading = false;
@@ -81,5 +85,8 @@ export class ExamResultsModalComponent implements OnInit {
   ionViewDidLeave() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
+  }
+  downloadpdf() {
+    this.link.click();
   }
 }
