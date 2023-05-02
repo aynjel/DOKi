3102653ee878672a6203bdf9329abe494bebac37
Jessie Jay Rubi <jessie_jay_rubi@chonghua.com.ph>
2023-05-02 13:38:27 +0800

    added fix on doki

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   //apiRouterUrl: 'http://10.130.21.166:5002/api/',
   apiRouterUrl: "http://10.151.12.120:7223/api/",
   //apiRouterUrlTest: 'http://localhost:5000',
-  apiRouterUrlTest: window["env"]["API_URL"],
+  apiRouterUrlTest: "https://api.chonghua.com.ph",
   linkRouterUrl: "http://10.128.18.112:9092/",
   //apiRouterUrl: 'http://10.130.21.214:59201/api/',
   apiResident: "http://10.151.12.120:7230/",
