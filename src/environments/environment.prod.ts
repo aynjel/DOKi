831bda80af3c4bc9d02e5ce8d0678d0b9c8204ad
Jessie Jay Rubi <jessie_jay_rubi@chonghua.com.ph>
2023-05-16 10:49:24 +0800

    test update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   production: true,
   // apiRouterUrl: 'https://doctorsportal.chonghua.com.ph/api/',
   apiRouterUrl: "https://doki.chonghua.com.ph/api/", // For IIS Deployment where Frontend and Backend are deployed in one server.
-  linkRouterUrl: "https://doki.chonghua.com.ph/",
+  linkRouterUrl: "https://www.chonghua.com.ph/api/",
   //apiRouterUrlTest: 'http://localhost:5000',
   apiRouterUrlTest: "https://api.chonghua.com.ph",
   // apiRouterUrl: 'http://10.128.18.112:9091/api/',
