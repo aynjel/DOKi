a9625181eba391ce48834fd55f87d8ad1560cb28
Jessie Jay Rubi <jessie_jay_rubi@chonghua.com.ph>
2023-07-19 09:54:36 +0800

    removed unused packages

--- src/app/chh-web-components/chh-app-change-password/chh-app-change-password.page.ts
+++ src/app/chh-web-components/chh-app-change-password/chh-app-change-password.page.ts
@@ -1,43 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController, AlertController, IonItem } from '@ionic/angular';
-import { AuthConstants, Consta } from '../../config/auth-constants';
+import { Component } from "@angular/core";
+import { ModalController, AlertController } from "@ionic/angular";
+import { Consta } from "../../config/auth-constants";
 import {
   AfterViewInit,
   ElementRef,
   Renderer2,
   Input,
   NgZone,
-} from '@angular/core';
-import { GestureController } from '@ionic/angular';
-import { Gesture, GestureConfig } from '@ionic/core';
-import { ViewChildren, QueryList } from '@angular/core';
-import { IonGrid, IonContent, IonRow } from '@ionic/angular';
-import { Constants } from '../../shared/constants';
-import { PatientService } from '../../services/patient/patient.service';
-import { FunctionsService } from 'src/app/shared/functions/functions.service';
-import { LoginData } from '../../models/login-data.model';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import * as bcrypt from 'bcryptjs';
+} from "@angular/core";
+import { GestureController } from "@ionic/angular";
+import { Constants } from "../../shared/constants";
+import { PatientService } from "../../services/patient/patient.service";
+import { FunctionsService } from "src/app/shared/functions/functions.service";
+import { CustomValidators } from "../../shared/custom-validators";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-import { CustomValidators } from '../../shared/custom-validators';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { PasswordStrengthValidator } from '../../shared/password-strength.validators';
-
-
-import { LoginModel,ChangePasswordModel,ChangePasswordModelV3 } from '../../models/patient';
-import { DoctorService } from 'src/app/services/doctor/doctor.service';
-
+import {
+  ChangePasswordModel,
+  ChangePasswordModelV3,
+} from "../../models/patient";
+import { DoctorService } from "src/app/services/doctor/doctor.service";
 
 @Component({
-  selector: 'app-chh-app-change-password',
-  templateUrl: './chh-app-change-password.page.html',
-  styleUrls: ['./chh-app-change-password.page.scss'],
+  selector: "app-chh-app-change-password",
+  templateUrl: "./chh-app-change-password.page.html",
+  styleUrls: ["./chh-app-change-password.page.scss"],
 })
 export class ChhAppChangePasswordPage {
   @Input() old_password: any;
   public form: FormGroup;
-  public changePasswordModel: ChangePasswordModel; 
-  public changePasswordModelV3: ChangePasswordModelV3; 
+  public changePasswordModel: ChangePasswordModel;
+  public changePasswordModelV3: ChangePasswordModelV3;
   TESTOldPassword;
   OldPassword;
   NewPassword;
@@ -52,7 +45,7 @@ export class ChhAppChangePasswordPage {
   isEyeOnOff4: Boolean = true;
   dr_username;
   errMessage;
-  btnDisable:boolean = false;
+  btnDisable: boolean = false;
   saltRounds = 10;
   public frmSignup: FormGroup;
   constructor(
@@ -70,7 +63,6 @@ export class ChhAppChangePasswordPage {
   ) {
     this.changePasswordModel = new ChangePasswordModel();
     this.changePasswordModel.mode = Consta.mode;
-
 
     // this.form = fb.group({
     //   password: ['', [Validators.required, PasswordStrengthValidator]],
@@ -108,7 +100,7 @@ export class ChhAppChangePasswordPage {
           ]),
         ],
         confirmPassword: [null, Validators.compose([Validators.required])],
-        oldPassword: ['', '']
+        oldPassword: ["", ""],
       },
       {
         // check whether our password and confirm password match
@@ -118,8 +110,7 @@ export class ChhAppChangePasswordPage {
   }
 
   ngOnInit() {
-
-    this.dr_username = atob(localStorage.getItem('username'));
+    this.dr_username = atob(localStorage.getItem("username"));
   }
 
   showPassword1() {
@@ -182,28 +173,28 @@ export class ChhAppChangePasswordPage {
    }*/
 
   public getType2() {
-    return this.isActiveToggleTextPassword2 ? 'password' : 'text';
+    return this.isActiveToggleTextPassword2 ? "password" : "text";
   }
 
   public getType3() {
-    return this.isActiveToggleTextPassword3 ? 'password' : 'text';
+    return this.isActiveToggleTextPassword3 ? "password" : "text";
   }
   public getType4() {
-    return this.isActiveToggleTextPassword4 ? 'password' : 'text';
+    return this.isActiveToggleTextPassword4 ? "password" : "text";
   }
   public getName2() {
-    return this.isEyeOnOff2 ? 'eye-off-outline' : 'eye-outline';
+    return this.isEyeOnOff2 ? "eye-off-outline" : "eye-outline";
   }
 
   public getName3() {
-    return this.isEyeOnOff3 ? 'eye-off-outline' : 'eye-outline';
+    return this.isEyeOnOff3 ? "eye-off-outline" : "eye-outline";
   }
   public getName4() {
-    return this.isEyeOnOff4 ? 'eye-off-outline' : 'eye-outline';
+    return this.isEyeOnOff4 ? "eye-off-outline" : "eye-outline";
   }
 
   async closeModal() {
-    await this.modalController.dismiss('None');
+    await this.modalController.dismiss("None");
   }
 
   /*<div class="alert-head sc-ion-alert-ios"><h2 id="alert-1-hdr" class="alert-title sc-ion-alert-ios">Use this lightsaber?</h2></div>
@@ -213,30 +204,30 @@ export class ChhAppChangePasswordPage {
     this.btnDisable = true;
     if (this.NewPassword != this.ConfirmPassword) {
       this.btnDisable = false;
-      this.errMessage = ' (passwords did not Match)';
-      let myDiv1 = document.getElementById('pWord1');
-      let myDiv2 = document.getElementById('pWord2');
-      myDiv1.style.color = 'red';
-      myDiv2.style.color = 'red';
+      this.errMessage = " (passwords did not Match)";
+      let myDiv1 = document.getElementById("pWord1");
+      let myDiv2 = document.getElementById("pWord2");
+      myDiv1.style.color = "red";
+      myDiv2.style.color = "red";
     } else {
-     // this.changePasswordModel = new ChangePasswordModel();
-      this.errMessage = '';
-      let myDiv1 = document.getElementById('pWord1');
-      let myDiv2 = document.getElementById('pWord2');
-      myDiv1.style.color = 'black';
-      myDiv2.style.color = 'black';
+      // this.changePasswordModel = new ChangePasswordModel();
+      this.errMessage = "";
+      let myDiv1 = document.getElementById("pWord1");
+      let myDiv2 = document.getElementById("pWord2");
+      myDiv1.style.color = "black";
+      myDiv2.style.color = "black";
       let hashedPassword;
-     // bcrypt.hash(this.NewPassword, this.saltRounds).then((hash) => {
-        hashedPassword = this.NewPassword;
-        this.changePasswordModel.appCode = 'DPP';
-        this.changePasswordModel.username =  this.dr_username;
-        this.changePasswordModel.oldPassword =   this.OldPassword;
-        this.changePasswordModel.newPassword = this.NewPassword;
-        //console.log(this.changePasswordModel);
-        this.changePasswordModelV3 = new ChangePasswordModelV3();
-        this.changePasswordModelV3.currentPassword = this.OldPassword;
-        this.changePasswordModelV3.newPassword = this.NewPassword;
-       /*  let resJson =
+      // bcrypt.hash(this.NewPassword, this.saltRounds).then((hash) => {
+      hashedPassword = this.NewPassword;
+      this.changePasswordModel.appCode = "DPP";
+      this.changePasswordModel.username = this.dr_username;
+      this.changePasswordModel.oldPassword = this.OldPassword;
+      this.changePasswordModel.newPassword = this.NewPassword;
+      //console.log(this.changePasswordModel);
+      this.changePasswordModelV3 = new ChangePasswordModelV3();
+      this.changePasswordModelV3.currentPassword = this.OldPassword;
+      this.changePasswordModelV3.newPassword = this.NewPassword;
+      /*  let resJson =
           '{"appCode": "DPP","username": "' +
           this.dr_username +
           '","oldPassword": "' +
@@ -244,39 +235,39 @@ export class ChhAppChangePasswordPage {
           '","newPassword":"' +
           hash +
           '"}'; */
-        let dJson;
-        //console.log(this.changePasswordModelV3);
-        
-        this.doctorService.changePasswordV3(this.changePasswordModelV3).subscribe(
-          (res: any) => {
-            dJson = res;
-          },
-          (error) => {
-            this.functionsService.sorryDoc();
-            this.btnDisable = false;
-          },
-          () => {
-            /*if (dJson.Message == 'Success') {
+      let dJson;
+      //console.log(this.changePasswordModelV3);
+
+      this.doctorService.changePasswordV3(this.changePasswordModelV3).subscribe(
+        (res: any) => {
+          dJson = res;
+        },
+        (error) => {
+          this.functionsService.sorryDoc();
+          this.btnDisable = false;
+        },
+        () => {
+          /*if (dJson.Message == 'Success') {
               this.modalController.dismiss(hashedPassword, this.NewPassword);
             } else {
               this.modalController.dismiss('Error');
             }*/
-            if(dJson.succeeded){
-              this.modalController.dismiss("Success");
-            }else{
-              if(dJson.errors[0].code == "PasswordMismatch"){
-                this.modalUpdateV3(
-                  this.constants.UI_COMPONENT_TEXT_VALUE_PASSWORD_FAILED_TITLE,
-                  dJson.errors[0].description,
-                  false
-                );
-              }
-              //this.modalController.dismiss(dJson.errors[0].description);
+          if (dJson.succeeded) {
+            this.modalController.dismiss("Success");
+          } else {
+            if (dJson.errors[0].code == "PasswordMismatch") {
+              this.modalUpdateV3(
+                this.constants.UI_COMPONENT_TEXT_VALUE_PASSWORD_FAILED_TITLE,
+                dJson.errors[0].description,
+                false
+              );
             }
-            this.btnDisable = false;
+            //this.modalController.dismiss(dJson.errors[0].description);
           }
-        );
-     // });
+          this.btnDisable = false;
+        }
+      );
+      // });
     }
     /*
     bcrypt.hash(resultJson.data, this.saltRounds).then(
@@ -292,29 +283,27 @@ export class ChhAppChangePasswordPage {
 */
   }
 
-  btnSubmit:boolean = false;
-  inputchange(){
-    let Opass:boolean;
-    if(this.OldPassword == '' || this.OldPassword == undefined){
+  btnSubmit: boolean = false;
+  inputchange() {
+    let Opass: boolean;
+    if (this.OldPassword == "" || this.OldPassword == undefined) {
       Opass = false;
-    }else{
+    } else {
       Opass = true;
     }
-    this.btnSubmit = (Opass && !this.frmSignup.invalid);
+    this.btnSubmit = Opass && !this.frmSignup.invalid;
   }
 
   async modalUpdateV3(header, message, data) {
     this.btnDisable = false;
     const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
+      cssClass: "my-custom-class",
       header: header,
       message: message,
       buttons: [
         {
-          text: 'Okay',
-          handler: () => {
-
-          },
+          text: "Okay",
+          handler: () => {},
         },
       ],
     });
--- src/app/pages/resetpassword/resetpassword.page.ts
+++ src/app/pages/resetpassword/resetpassword.page.ts
@@ -1,47 +1,35 @@
-import { HttpParams } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { ModalController, AlertController, IonItem } from '@ionic/angular';
-import { AuthConstants, Consta } from '../../config/auth-constants';
+import { ActivatedRoute, Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { ModalController, AlertController } from "@ionic/angular";
+import { Consta } from "../../config/auth-constants";
+import { ElementRef, Renderer2, Input, NgZone } from "@angular/core";
+import { GestureController } from "@ionic/angular";
+import { Constants } from "../../shared/constants";
+import { PatientService } from "../../services/patient/patient.service";
+import { FunctionsService } from "src/app/shared/functions/functions.service";
+
+import { CustomValidators } from "../../shared/custom-validators";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ResetPasswordV3 } from "../../models/doctor";
+
 import {
-  AfterViewInit,
-  ElementRef,
-  Renderer2,
-  Input,
-  NgZone,
-} from '@angular/core';
-import { GestureController } from '@ionic/angular';
-import { Gesture, GestureConfig } from '@ionic/core';
-import { ViewChildren, QueryList } from '@angular/core';
-import { IonGrid, IonContent, IonRow } from '@ionic/angular';
-import { Constants } from '../../shared/constants';
-import { PatientService } from '../../services/patient/patient.service';
-import { FunctionsService } from 'src/app/shared/functions/functions.service';
-import { LoginData } from '../../models/login-data.model';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import * as bcrypt from 'bcryptjs';
-
-import { CustomValidators } from '../../shared/custom-validators';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { PasswordStrengthValidator } from '../../shared/password-strength.validators';
-import { ResetPasswordV3} from '../../models/doctor';
-
-
-import { LoginModel,ChangePasswordModel,ChangePasswordModelV3 } from '../../models/patient';
-import { DoctorService } from 'src/app/services/doctor/doctor.service';
+  LoginModel,
+  ChangePasswordModel,
+  ChangePasswordModelV3,
+} from "../../models/patient";
+import { DoctorService } from "src/app/services/doctor/doctor.service";
 
 @Component({
-  selector: 'app-resetpassword',
-  templateUrl: './resetpassword.page.html',
-  styleUrls: ['./resetpassword.page.scss'],
+  selector: "app-resetpassword",
+  templateUrl: "./resetpassword.page.html",
+  styleUrls: ["./resetpassword.page.scss"],
 })
 export class ResetpasswordPage implements OnInit {
-
   @Input() old_password: any;
   public form: FormGroup;
-  public changePasswordModel: ChangePasswordModel; 
-  public changePasswordModelV3: ChangePasswordModelV3; 
-  resetPasswordV3 : ResetPasswordV3;
+  public changePasswordModel: ChangePasswordModel;
+  public changePasswordModelV3: ChangePasswordModelV3;
+  resetPasswordV3: ResetPasswordV3;
   TESTOldPassword;
   OldPassword;
   NewPassword;
@@ -56,7 +44,7 @@ export class ResetpasswordPage implements OnInit {
   isEyeOnOff4: Boolean = true;
   dr_username;
   errMessage;
-  btnDisable:boolean = false;
+  btnDisable: boolean = false;
   saltRounds = 10;
   public frmSignup: FormGroup;
   constructor(
@@ -72,11 +60,10 @@ export class ResetpasswordPage implements OnInit {
     private fb: FormBuilder,
     private doctorService: DoctorService,
     private router: Router,
-    private activatedRoute:ActivatedRoute
+    private activatedRoute: ActivatedRoute
   ) {
     this.changePasswordModel = new ChangePasswordModel();
     this.changePasswordModel.mode = Consta.mode;
-
 
     // this.form = fb.group({
     //   password: ['', [Validators.required, PasswordStrengthValidator]],
@@ -85,44 +72,39 @@ export class ResetpasswordPage implements OnInit {
   }
 
   createSignupForm(): FormGroup {
-    return this.fb.group(
-      {
-        password: [
-          null,
-          Validators.compose([
-            Validators.required,
-            // check whether the entered password has a number
-            CustomValidators.patternValidator(/\d/, {
-              hasNumber: true,
-            }),
-            // check whether the entered password has upper case letter
-            CustomValidators.patternValidator(/[A-Z]/, {
-              hasCapitalCase: true,
-            }),
-            // check whether the entered password has a lower case letter
-            CustomValidators.patternValidator(/[a-z]/, {
-              hasSmallCase: true,
-            }),
-            // check whether the entered password has a special character
-            CustomValidators.patternValidator(
-              /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
-              {
-                hasSpecialCharacters: true,
-              }
-            ),
-            Validators.minLength(8),
-          ]),
-        ],
-        confirmPassword: [null, Validators.compose([Validators.required])],
-        oldPassword: ['', '']
-      }
-    );
+    return this.fb.group({
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          // check whether the entered password has a number
+          CustomValidators.patternValidator(/\d/, {
+            hasNumber: true,
+          }),
+          // check whether the entered password has upper case letter
+          CustomValidators.patternValidator(/[A-Z]/, {
+            hasCapitalCase: true,
+          }),
+          // check whether the entered password has a lower case letter
+          CustomValidators.patternValidator(/[a-z]/, {
+            hasSmallCase: true,
+          }),
+          // check whether the entered password has a special character
+          CustomValidators.patternValidator(
+            /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+            {
+              hasSpecialCharacters: true,
+            }
+          ),
+          Validators.minLength(8),
+        ]),
+      ],
+      confirmPassword: [null, Validators.compose([Validators.required])],
+      oldPassword: ["", ""],
+    });
   }
 
-  ngOnInit() {
-
-   
-  }
+  ngOnInit() {}
 
   showPassword1() {
     this.isActiveToggleTextPassword2 =
@@ -184,28 +166,28 @@ export class ResetpasswordPage implements OnInit {
    }*/
 
   public getType2() {
-    return this.isActiveToggleTextPassword2 ? 'password' : 'text';
+    return this.isActiveToggleTextPassword2 ? "password" : "text";
   }
 
   public getType3() {
-    return this.isActiveToggleTextPassword3 ? 'password' : 'text';
+    return this.isActiveToggleTextPassword3 ? "password" : "text";
   }
   public getType4() {
-    return this.isActiveToggleTextPassword4 ? 'password' : 'text';
+    return this.isActiveToggleTextPassword4 ? "password" : "text";
   }
   public getName2() {
-    return this.isEyeOnOff2 ? 'eye-off-outline' : 'eye-outline';
+    return this.isEyeOnOff2 ? "eye-off-outline" : "eye-outline";
   }
 
   public getName3() {
-    return this.isEyeOnOff3 ? 'eye-off-outline' : 'eye-outline';
+    return this.isEyeOnOff3 ? "eye-off-outline" : "eye-outline";
   }
   public getName4() {
-    return this.isEyeOnOff4 ? 'eye-off-outline' : 'eye-outline';
+    return this.isEyeOnOff4 ? "eye-off-outline" : "eye-outline";
   }
 
   async closeModal() {
-    this.router.navigate(['/login']);
+    this.router.navigate(["/login"]);
   }
 
   /*<div class="alert-head sc-ion-alert-ios"><h2 id="alert-1-hdr" class="alert-title sc-ion-alert-ios">Use this lightsaber?</h2></div>
@@ -214,62 +196,56 @@ export class ResetpasswordPage implements OnInit {
   save() {
     this.btnDisable = true;
     if (this.NewPassword != this.ConfirmPassword) {
-      this.errMessage = ' (passwords did not Match)';
-      let myDiv1 = document.getElementById('pWord1');
-      let myDiv2 = document.getElementById('pWord2');
-      myDiv1.style.color = 'red';
-      myDiv2.style.color = 'red';
+      this.errMessage = " (passwords did not Match)";
+      let myDiv1 = document.getElementById("pWord1");
+      let myDiv2 = document.getElementById("pWord2");
+      myDiv1.style.color = "red";
+      myDiv2.style.color = "red";
       this.btnDisable = false;
     } else {
+      this.errMessage = "";
+      let myDiv1 = document.getElementById("pWord1");
+      let myDiv2 = document.getElementById("pWord2");
+      myDiv1.style.color = "black";
+      myDiv2.style.color = "black";
 
-      this.errMessage = '';
-      let myDiv1 = document.getElementById('pWord1');
-      let myDiv2 = document.getElementById('pWord2');
-      myDiv1.style.color = 'black';
-      myDiv2.style.color = 'black';
-
-     // bcrypt.hash(this.NewPassword, this.saltRounds).then((hash) => {
+      // bcrypt.hash(this.NewPassword, this.saltRounds).then((hash) => {
 
       this.resetPasswordV3 = new ResetPasswordV3();
-      this.activatedRoute.queryParams.subscribe(params  => {
+      this.activatedRoute.queryParams.subscribe((params) => {
         console.log(params);
-        console.log(params['token']);
-        this.resetPasswordV3.token =params['token'];
-        this.resetPasswordV3.email =params['email'];
+        console.log(params["token"]);
+        this.resetPasswordV3.token = params["token"];
+        this.resetPasswordV3.email = params["email"];
         console.log(this.resetPasswordV3);
-        
-    });
+      });
 
-        this.resetPasswordV3.password = this.NewPassword;
-        this.resetPasswordV3.confirmPassword = this.ConfirmPassword;
-        console.log(this.resetPasswordV3);
-        
-        this.doctorService.resetPasswordV3(this.resetPasswordV3).subscribe(
-          (res: any) => {
-            console.log(res);
+      this.resetPasswordV3.password = this.NewPassword;
+      this.resetPasswordV3.confirmPassword = this.ConfirmPassword;
+      console.log(this.resetPasswordV3);
 
-          },
-          (error) => {
-            this.btnDisable = false;
-            this.modalUpdate(
-              this.constants.UI_COMPONENT_TEXT_VALUE_PASSWORD_FAILED_TITLE,
-              this.constants.UI_COMPONENT_TEXT_VALUE_UPDATE_PASSWORD_FAILED_BODY,
-              true
-            );
-          },
-          () => {
-            this.modalUpdate(
-              this.constants.UI_COMPONENT_TEXT_VALUE_PASSWORD_SUCCESS_TITLE,
-              this.constants.UI_COMPONENT_TEXT_VALUE_CHANGE_PASSWORD_SUCCESS_BODY,
-              true
-            );
-          }
-        );
+      this.doctorService.resetPasswordV3(this.resetPasswordV3).subscribe(
+        (res: any) => {
+          console.log(res);
+        },
+        (error) => {
+          this.btnDisable = false;
+          this.modalUpdate(
+            this.constants.UI_COMPONENT_TEXT_VALUE_PASSWORD_FAILED_TITLE,
+            this.constants.UI_COMPONENT_TEXT_VALUE_UPDATE_PASSWORD_FAILED_BODY,
+            true
+          );
+        },
+        () => {
+          this.modalUpdate(
+            this.constants.UI_COMPONENT_TEXT_VALUE_PASSWORD_SUCCESS_TITLE,
+            this.constants.UI_COMPONENT_TEXT_VALUE_CHANGE_PASSWORD_SUCCESS_BODY,
+            true
+          );
+        }
+      );
 
-
-
-
-     // });
+      // });
     }
     /*
     bcrypt.hash(resultJson.data, this.saltRounds).then(
@@ -287,21 +263,19 @@ export class ResetpasswordPage implements OnInit {
 
   async modalUpdate(header, message, data) {
     const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
+      cssClass: "my-custom-class",
       backdropDismiss: false,
       header: header,
       message: message,
       buttons: [
         {
-          text: 'Okay',
+          text: "Okay",
           handler: () => {
-            this.router.navigate(['/login']);
-  
+            this.router.navigate(["/login"]);
           },
         },
       ],
     });
     await alert.present();
   }
-
 }
