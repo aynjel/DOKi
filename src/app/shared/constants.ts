3b3ab4054e64fddb097dca1895b58d733d172dc4
Jessie Jay Rubi <jessie_jay_rubi@chonghua.com.ph>
2023-11-20 10:14:03 +0800

    bumped version

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -161,7 +161,7 @@ export class Constants {
   /**
    * Version
    */
-  public DOCTOR_PORTAL__VALUE__VERSION: string = "3.8.3";
+  public DOCTOR_PORTAL__VALUE__VERSION: string = "3.9.0";
 
   // (W)
   /**
